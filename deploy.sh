e458bb75fe853fc8bc90fff2c7c0260700cc2183
volter9 <volter925@gmail.com>
2015-09-26 19:13:02 -0700

    Lazy load

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,23 @@ source ./ftp.sh
 
 DOMAIN="$1"
 LANG="$2"
+FILES=$(git diff --name-only HEAD~1 HEAD)
+
+if [ "$3" == "true" ]
+then
+    FILES=$(git ls-files)
+fi
+
+FILES=$(echo "$FILES" | grep ^assets | awk '{print "put -z ./" $0 " ./" $0}')
 
 ncftp <<EOF
 open -u '$USER' -p '$PASS' '$HOST'
 cd /www/pomidorko.$DOMAIN 
+mkdir assets
+mkdir assets/js
+mkdir assets/css
+mkdir assets/img
+mkdir assets/sounds
 put -zf ./build/$LANG.html index.php
-put -rf ./assets
+$FILES
 EOF
